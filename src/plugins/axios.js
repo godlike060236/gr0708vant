e3fb2f9297ccc2c607a297fed6ee148e75102ea7
godlike060236 <962179903@qq.com>
2021-07-14 18:00:03 +0800

    Changes by lenovo

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,7 @@
 
 import Vue from 'vue';
 import axios from "axios";
+import {Notify} from "vant";
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 存储到sessionStorage；storage into sessionStorage
+  plugins: [createPersistedState({
+    storage: window.sessionStorage
+  })],
   state: {
+    token: ''
   },
   mutations: {
+    SET_TOKEN(state, token) {
+      state.token = token
+    }
   },
-  actions: {
-  },
-  modules: {
+  getters: {
+    GET_TOKEN(state) {
+      return state.token
+    }
   }
 })
